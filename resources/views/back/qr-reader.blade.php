012cd242a01377f72d2fc084e4ecebe4dde0f00d
Logan <logannaranjo4@gmail.com>
2025-05-14 18:23:53 +0200

    Qr reader View Style

--- resources/views/back/qr-reader.blade.php
+++ resources/views/back/qr-reader.blade.php
@@ -1,8 +1,8 @@
 @extends('_partials.layout')
-@section('titlePage', 'Agregar administrador')
+@section('titlePage', 'Escáner QR')
 
 @section('content')
     <div class="QRContainerReader p-5 flex gap-3 flex-col h-full justify-center">
-        <div id="reader" width="600px"></div>
+        <div id="reader" class="w-[400px] lg:w-[500px]"></div>
     </div>
 @endsection
