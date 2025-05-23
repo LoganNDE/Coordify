4749c4db00643a96a16fcfc80bb34574a8b11929
Logan <logannaranjo4@gmail.com>
2025-05-23 20:06:23 +0200

    Coordify V1.0

--- database/seeders/CrearUsuarioSeeder.php
+++ database/seeders/CrearUsuarioSeeder.php
@@ -14,30 +14,10 @@ class CrearUsuarioSeeder extends Seeder
     public function run(): void
     {
         $user = new User();
-        $user->name = 'logan';
+        $user->name = 'admin';
         $user->email = 'admin@admin.com';
-        $user->password = bcrypt('admin');
+        $user->password = bcrypt('Seiscerotres1$');
         $user->timestamps = now();
-        $user->subscription_id = 4;
-        $user->stripe_subscription_id = 'sub_1Q2345678901234567890123';
         $user->save();
-
-        $user2 = new User();
-        $user2->name = 'admin';
-        $user2->email = 'admin2@admin.com';
-        $user2->password = bcrypt('admin');
-        $user2->subscription_id = 4;
-        $user2->stripe_subscription_id = 'sub_1Q2345678901234567890123';
-        $user2->timestamps = now();
-        $user2->save();
-
-        $user3 = new User();
-        $user3->name = 'paco';
-        $user3->email = 'paco@paco.com';
-        $user3->password = bcrypt('admin');
-        $user3->subscription_id = 4;
-        $user3->stripe_subscription_id = 'sub_1Q2345678901234567890123';
-        $user3->timestamps = now();
-        $user3->save();
     }
 }
